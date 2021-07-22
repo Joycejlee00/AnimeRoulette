c934a559f4f27507e5f3b6f14d3550447a7bc823
Joycejlee00 <joycelee1294@gmail.com>
2021-07-21 22:44:29 -0400

    update icon

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ bot.on('ready', () => {
     console.log('logged in!');
 
 
-    avatar = "/pics/icon.png";
+    avatar = "https://media.discordapp.net/attachments/867500059812823071/867597825319895040/icon.png";
     bot.user.setAvatar(avatar);
 });
 
